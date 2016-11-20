0402b042208a396292fa08c906cd55717a9dbc48
Tapani Otala <github@tjotala.com>
2016-11-19 17:39:13 -0800

    Make OnComplete a virtual function to be overridden by whoever cares

--- NeoPatterns.h
+++ NeoPatterns.h
@@ -21,13 +21,13 @@ class NeoPatterns : public Adafruit_NeoPixel
     uint16_t TotalSteps;  // total number of steps in the pattern
     uint16_t Index;  // current step within the pattern
     
-    void (*OnComplete)();  // Callback on completion of pattern
-    
     // Constructor - calls base-class constructor to initialize strip
-    NeoPatterns(uint16_t pixels, uint8_t pin, uint8_t type, void (*callback)())
+    NeoPatterns(uint16_t pixels, uint8_t pin, uint8_t type = NEO_GRB + NEO_KHZ800)
     :Adafruit_NeoPixel(pixels, pin, type)
     {
-        OnComplete = callback;
+    }
+
+    virtual void OnComplete() {
     }
     
     // Update the pattern
@@ -68,10 +68,7 @@ class NeoPatterns : public Adafruit_NeoPixel
            if (Index >= TotalSteps)
             {
                 Index = 0;
-                if (OnComplete != NULL)
-                {
-                    OnComplete(); // call the completion callback
-                }
+                OnComplete(); // call the completion callback
             }
         }
         else // Direction == REVERSE
@@ -80,10 +77,7 @@ class NeoPatterns : public Adafruit_NeoPixel
             if (Index <= 0)
             {
                 Index = TotalSteps-1;
-                if (OnComplete != NULL)
-                {
-                    OnComplete(); // call the completion callback
-                }
+                OnComplete(); // call the completion callback
             }
         }
     }
@@ -288,118 +282,3 @@ class NeoPatterns : public Adafruit_NeoPixel
         }
     }
 };
-
-#ifdef DEMO
-void Ring1Complete();
-void Ring2Complete();
-void StickComplete();
-
-// Define some NeoPatterns for the two rings and the stick
-//  as well as some completion routines
-NeoPatterns Ring1(24, 5, NEO_GRB + NEO_KHZ800, &Ring1Complete);
-NeoPatterns Ring2(16, 6, NEO_GRB + NEO_KHZ800, &Ring2Complete);
-NeoPatterns Stick(16, 7, NEO_GRB + NEO_KHZ800, &StickComplete);
-
-// Initialize everything and prepare to start
-void setup()
-{
-  Serial.begin(115200);
-
-   pinMode(8, INPUT_PULLUP);
-   pinMode(9, INPUT_PULLUP);
-    
-    // Initialize all the pixelStrips
-    Ring1.begin();
-    Ring2.begin();
-    Stick.begin();
-    
-    // Kick off a pattern
-    Ring1.TheaterChase(Ring1.Color(255,255,0), Ring1.Color(0,0,50), 100);
-    Ring2.RainbowCycle(3);
-    Ring2.Color1 = Ring1.Color1;
-    Stick.Scanner(Ring1.Color(255,0,0), 55);
-}
-
-// Main loop
-void loop()
-{
-    // Update the rings.
-    Ring1.Update();
-    Ring2.Update();    
-    
-    // Switch patterns on a button press:
-    if (digitalRead(8) == LOW) // Button #1 pressed
-    {
-        // Switch Ring1 to FASE pattern
-        Ring1.ActivePattern = FADE;
-        Ring1.Interval = 20;
-        // Speed up the rainbow on Ring2
-        Ring2.Interval = 0;
-        // Set stick to all red
-        Stick.ColorSet(Stick.Color(255, 0, 0));
-    }
-    else if (digitalRead(9) == LOW) // Button #2 pressed
-    {
-        // Switch to alternating color wipes on Rings1 and 2
-        Ring1.ActivePattern = COLOR_WIPE;
-        Ring2.ActivePattern = COLOR_WIPE;
-        Ring2.TotalSteps = Ring2.numPixels();
-        // And update tbe stick
-        Stick.Update();
-    }
-    else // Back to normal operation
-    {
-        // Restore all pattern parameters to normal values
-        Ring1.ActivePattern = THEATER_CHASE;
-        Ring1.Interval = 100;
-        Ring2.ActivePattern = RAINBOW_CYCLE;
-        Ring2.TotalSteps = 255;
-        Ring2.Interval = min(10, Ring2.Interval);
-        // And update tbe stick
-        Stick.Update();
-    }    
-}
-
-//------------------------------------------------------------
-//Completion Routines - get called on completion of a pattern
-//------------------------------------------------------------
-
-// Ring1 Completion Callback
-void Ring1Complete()
-{
-    if (digitalRead(9) == LOW)  // Button #2 pressed
-    {
-        // Alternate color-wipe patterns with Ring2
-        Ring2.Interval = 40;
-        Ring1.Color1 = Ring1.Wheel(random(255));
-        Ring1.Interval = 20000;
-    }
-    else  // Retrn to normal
-    {
-      Ring1.Reverse();
-    }
-}
-
-// Ring 2 Completion Callback
-void Ring2Complete()
-{
-    if (digitalRead(9) == LOW)  // Button #2 pressed
-    {
-        // Alternate color-wipe patterns with Ring1
-        Ring1.Interval = 20;
-        Ring2.Color1 = Ring2.Wheel(random(255));
-        Ring2.Interval = 20000;
-    }
-    else  // Retrn to normal
-    {
-        Ring2.RainbowCycle(random(0,10));
-    }
-}
-
-// Stick Completion Callback
-void StickComplete()
-{
-    // Random color change for next scan
-    Stick.Color1 = Stick.Wheel(random(255));
-}
-#endif // #ifdef DEMO
