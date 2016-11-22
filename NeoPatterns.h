9cba4bf5c0f97094e6fec8c9dbfbdcddfeda4280
Tapani Otala <github@tjotala.com>
2016-11-21 16:30:25 -0800

    Substantial re-factoring for better extensibility & modularity.

--- NeoPatterns.h
+++ NeoPatterns.h
@@ -1,242 +1,140 @@
 #include <Adafruit_NeoPixel.h>
 
-// Pattern types supported:
-enum  pattern { NONE, RAINBOW_CYCLE, THEATER_CHASE, COLOR_WIPE, SCANNER, FADE };
-// Patern directions supported:
-enum  direction { FORWARD, REVERSE };
+typedef unsigned long       millis_t;
+typedef int8_t              direction_t;
+typedef uint32_t            color_t;
 
-// NeoPattern Class - derived from the Adafruit_NeoPixel class
-class NeoPatterns : public Adafruit_NeoPixel
+class NeoPatterns; // forward decl
+
+class NeoPattern
 {
-    public:
+public:
+    static const direction_t FORWARD = 1;
+    static const direction_t REVERSE = -1;
 
-    // Member Variables:  
-    pattern  ActivePattern;  // which pattern is running
-    direction Direction;     // direction to run the pattern
-    
-    unsigned long Interval;   // milliseconds between updates
-    unsigned long lastUpdate; // last update of position
-    
-    uint32_t Color1, Color2;  // What colors are in use
-    uint16_t TotalSteps;  // total number of steps in the pattern
-    uint16_t Index;  // current step within the pattern
-    
-    // Constructor - calls base-class constructor to initialize strip
-    NeoPatterns(uint16_t pixels, uint8_t pin, uint8_t type = NEO_GRB + NEO_KHZ800)
-    :Adafruit_NeoPixel(pixels, pin, type)
+    NeoPattern(NeoPatterns& pixels, millis_t interval, color_t color1, color_t color2, uint16_t totalSteps, direction_t dir = FORWARD) :
+      Pixels(pixels),
+      Interval(interval),
+      Color1(color1),
+      Color2(color2),
+      TotalSteps(totalSteps),
+      Direction(dir)
     {
+        Reset();
     }
 
-    virtual void OnComplete() {
+    void Reset()
+    {
+        Index = LastUpdate = 0;
+    }
+
+    bool ShouldUpdate() const
+    {
+        return millis() - LastUpdate > Interval;
+    }
+
+    void AfterUpdate()
+    {
+        LastUpdate = millis();
+        Increment();
+    }
+
+    // Completion callback
+    virtual void OnComplete()
+    {
     }
     
     // Update the pattern
-    void Update()
-    {
-        if((millis() - lastUpdate) > Interval) // time to update
-        {
-            lastUpdate = millis();
-            switch(ActivePattern)
-            {
-                case RAINBOW_CYCLE:
-                    RainbowCycleUpdate();
-                    break;
-                case THEATER_CHASE:
-                    TheaterChaseUpdate();
-                    break;
-                case COLOR_WIPE:
-                    ColorWipeUpdate();
-                    break;
-                case SCANNER:
-                    ScannerUpdate();
-                    break;
-                case FADE:
-                    FadeUpdate();
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-  
+    virtual void Update() = 0;
+
     // Increment the Index and reset at the end
     void Increment()
     {
-        if (Direction == FORWARD)
+        Index += Direction;
+        if (Index >= TotalSteps)
         {
-           Index++;
-           if (Index >= TotalSteps)
-            {
-                Index = 0;
-                OnComplete(); // call the completion callback
-            }
+            Index = 0;
+            OnComplete();
         }
-        else // Direction == REVERSE
+        else if (Index <= 0)
         {
-            --Index;
-            if (Index <= 0)
-            {
-                Index = TotalSteps-1;
-                OnComplete(); // call the completion callback
-            }
+            Index = TotalSteps - 1;
+            OnComplete();
         }
     }
     
     // Reverse pattern direction
     void Reverse()
     {
-        if (Direction == FORWARD)
-        {
-            Direction = REVERSE;
-            Index = TotalSteps-1;
-        }
-        else
-        {
-            Direction = FORWARD;
-            Index = 0;
-        }
-    }
-    
-    // Initialize for a RainbowCycle
-    void RainbowCycle(uint8_t interval, direction dir = FORWARD)
-    {
-        ActivePattern = RAINBOW_CYCLE;
-        Interval = interval;
-        TotalSteps = 255;
-        Index = 0;
-        Direction = dir;
-    }
-    
-    // Update the Rainbow Cycle Pattern
-    void RainbowCycleUpdate()
-    {
-        for(int i=0; i< numPixels(); i++)
-        {
-            setPixelColor(i, Wheel(((i * 256 / numPixels()) + Index) & 255));
-        }
-        show();
-        Increment();
+        Direction *= -1;
+        Index = (Direction == FORWARD) ? 0 : TotalSteps - 1;
     }
 
-    // Initialize for a Theater Chase
-    void TheaterChase(uint32_t color1, uint32_t color2, uint8_t interval, direction dir = FORWARD)
-    {
-        ActivePattern = THEATER_CHASE;
-        Interval = interval;
-        TotalSteps = numPixels();
-        Color1 = color1;
-        Color2 = color2;
-        Index = 0;
-        Direction = dir;
-   }
+    NeoPatterns& Pixels;        // Which pixels to manipulate
+    millis_t Interval;          // Milliseconds between updates
+    millis_t LastUpdate;        // last update of position
+    color_t Color1, Color2;     // What color(s) to use
+    uint16_t TotalSteps;        // Total number of steps in the pattern
+    uint16_t Index;             // Current step within the pattern
+    direction_t Direction;      // Direction to run the pattern
+};
+
+// NeoPattern Class - derived from the Adafruit_NeoPixel class
+class NeoPatterns : public Adafruit_NeoPixel
+{
+public:
+    NeoPattern* ActivePattern;  // which pattern is running
     
-    // Update the Theater Chase Pattern
-    void TheaterChaseUpdate()
+    // Constructor - calls base-class constructor to initialize strip
+    NeoPatterns(uint16_t pixels, uint8_t pin, uint8_t type = NEO_GRB + NEO_KHZ800) :
+      Adafruit_NeoPixel(pixels, pin, type),
+      ActivePattern(NULL)
     {
-        for(int i=0; i< numPixels(); i++)
-        {
-            if ((i + Index) % 3 == 0)
-            {
-                setPixelColor(i, Color1);
-            }
-            else
-            {
-                setPixelColor(i, Color2);
-            }
-        }
-        show();
-        Increment();
     }
 
-    // Initialize for a ColorWipe
-    void ColorWipe(uint32_t color, uint8_t interval, direction dir = FORWARD)
+    bool IsActive() const
     {
-        ActivePattern = COLOR_WIPE;
-        Interval = interval;
-        TotalSteps = numPixels();
-        Color1 = color;
-        Index = 0;
-        Direction = dir;
-    }
-    
-    // Update the Color Wipe Pattern
-    void ColorWipeUpdate()
-    {
-        setPixelColor(Index, Color1);
-        show();
-        Increment();
-    }
-    
-    // Initialize for a SCANNNER
-    void Scanner(uint32_t color1, uint8_t interval)
-    {
-        ActivePattern = SCANNER;
-        Interval = interval;
-        TotalSteps = (numPixels() - 1) * 2;
-        Color1 = color1;
-        Index = 0;
+        return ActivePattern != NULL;
     }
 
-    // Update the Scanner Pattern
-    void ScannerUpdate()
-    { 
-        for (int i = 0; i < numPixels(); i++)
+    void Start(NeoPattern* pattern)
+    {
+        ActivePattern = pattern;
+        ActivePattern->Reset();
+    }
+
+    void Stop()
+    {
+        ActivePattern = NULL;
+        clear();
+        show();
+    }
+
+    // Update the pattern
+    void Update()
+    {
+        if (IsActive())
         {
-            if (i == Index)  // Scan Pixel to the right
+            if (ActivePattern->ShouldUpdate())
             {
-                 setPixelColor(i, Color1);
-            }
-            else if (i == TotalSteps - Index) // Scan Pixel to the left
-            {
-                 setPixelColor(i, Color1);
-            }
-            else // Fading tail
-            {
-                 setPixelColor(i, DimColor(getPixelColor(i)));
+                ActivePattern->Update();
+                show();
+                ActivePattern->AfterUpdate();
             }
         }
-        show();
-        Increment();
     }
-    
-    // Initialize for a Fade
-    void Fade(uint32_t color1, uint32_t color2, uint16_t steps, uint8_t interval, direction dir = FORWARD)
-    {
-        ActivePattern = FADE;
-        Interval = interval;
-        TotalSteps = steps;
-        Color1 = color1;
-        Color2 = color2;
-        Index = 0;
-        Direction = dir;
-    }
-    
-    // Update the Fade Pattern
-    void FadeUpdate()
-    {
-        // Calculate linear interpolation between Color1 and Color2
-        // Optimise order of operations to minimize truncation error
-        uint8_t red = ((Red(Color1) * (TotalSteps - Index)) + (Red(Color2) * Index)) / TotalSteps;
-        uint8_t green = ((Green(Color1) * (TotalSteps - Index)) + (Green(Color2) * Index)) / TotalSteps;
-        uint8_t blue = ((Blue(Color1) * (TotalSteps - Index)) + (Blue(Color2) * Index)) / TotalSteps;
-        
-        ColorSet(Color(red, green, blue));
-        show();
-        Increment();
-    }
-   
-    // Calculate 50% dimmed version of a color (used by ScannerUpdate)
-    uint32_t DimColor(uint32_t color)
+  
+    // Calculate 50% dimmed version of a color (used by Scanner::Update)
+    static color_t DimColor(color_t color)
     {
         // Shift R, G and B components one bit to the right
-        uint32_t dimColor = Color(Red(color) >> 1, Green(color) >> 1, Blue(color) >> 1);
-        return dimColor;
+        return Color(Red(color) >> 1, Green(color) >> 1, Blue(color) >> 1);
     }
 
     // Set all pixels to a color (synchronously)
-    void ColorSet(uint32_t color)
+    void ColorSet(color_t color)
     {
-        for (int i = 0; i < numPixels(); i++)
+        for (int i = 0; i < numPixels(); ++i)
         {
             setPixelColor(i, color);
         }
@@ -244,33 +142,33 @@ class NeoPatterns : public Adafruit_NeoPixel
     }
 
     // Returns the Red component of a 32-bit color
-    uint8_t Red(uint32_t color)
+    static uint8_t Red(color_t color)
     {
         return (color >> 16) & 0xFF;
     }
 
     // Returns the Green component of a 32-bit color
-    uint8_t Green(uint32_t color)
+    static uint8_t Green(color_t color)
     {
         return (color >> 8) & 0xFF;
     }
 
     // Returns the Blue component of a 32-bit color
-    uint8_t Blue(uint32_t color)
+    static uint8_t Blue(color_t color)
     {
         return color & 0xFF;
     }
     
     // Input a value 0 to 255 to get a color value.
     // The colours are a transition r - g - b - back to r.
-    uint32_t Wheel(byte WheelPos)
+    static color_t Wheel(byte WheelPos)
     {
         WheelPos = 255 - WheelPos;
-        if(WheelPos < 85)
+        if (WheelPos < 85)
         {
             return Color(255 - WheelPos * 3, 0, WheelPos * 3);
         }
-        else if(WheelPos < 170)
+        else if (WheelPos < 170)
         {
             WheelPos -= 85;
             return Color(0, WheelPos * 3, 255 - WheelPos * 3);
@@ -280,5 +178,90 @@ class NeoPatterns : public Adafruit_NeoPixel
             WheelPos -= 170;
             return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
         }
+    }
+};
+
+class RainbowCycle : public NeoPattern
+{
+public:
+    RainbowCycle(NeoPatterns& pixels, millis_t interval, direction_t dir = FORWARD) :
+      NeoPattern(pixels, interval, 0, 0, 255, dir)
+    {
+    }
+    
+    virtual void Update()
+    {
+        for (int i = 0; i < Pixels.numPixels(); ++i)
+        {
+            Pixels.setPixelColor(i, Pixels.Wheel(((i * 256 / Pixels.numPixels()) + Index) & 255));
+        }
+    }
+};
+
+class TheaterChase : public NeoPattern
+{
+public:
+    TheaterChase(NeoPatterns& pixels, millis_t interval, color_t color1, color_t color2, direction_t dir = FORWARD) :
+      NeoPattern(pixels, interval, color1, color2, pixels.numPixels(), dir)
+    {
+    }
+    
+    virtual void Update()
+    {
+        for (int i = 0; i < Pixels.numPixels(); ++i)
+        {
+            Pixels.setPixelColor(i, ((i + Index) % 3 == 0) ? Color1 : Color2);
+        }
+    }
+};
+
+class ColorWipe : public NeoPattern
+{
+public:
+    ColorWipe(NeoPatterns& pixels, millis_t interval, color_t color, direction_t dir = FORWARD) :
+      NeoPattern(pixels, interval, color, 0, pixels.numPixels(), dir)
+    {
+    }
+
+    virtual void Update()
+    {
+        Pixels.setPixelColor(Index, Color1);
+    }
+};
+
+class Scanner : public NeoPattern
+{
+public:
+    Scanner(NeoPatterns& pixels, millis_t interval, color_t color, bool split = false) :
+      NeoPattern(pixels, interval, color, 0, pixels.numPixels() * (split ? 1 : 2))
+    {
+    }
+
+    virtual void Update()
+    { 
+        for (int i = 0; i < Pixels.numPixels(); ++i)
+        {
+            Pixels.setPixelColor(i, (i == Index || i == TotalSteps - Index) ? Color1 : Pixels.DimColor(Pixels.getPixelColor(i)));
+        }
+    }
+};
+
+class Fade : public NeoPattern
+{
+public:
+    Fade(NeoPatterns& pixels, millis_t interval, color_t color1, color_t color2, uint16_t steps, direction_t dir = FORWARD) :
+      NeoPattern(pixels, interval, color1, color2, steps, dir)
+    {
+    }
+    
+    virtual void Update()
+    {
+        // Calculate linear interpolation between Color1 and Color2
+        // Optimise order of operations to minimize truncation error
+        uint8_t red = ((Pixels.Red(Color1) * (TotalSteps - Index)) + (Pixels.Red(Color2) * Index)) / TotalSteps;
+        uint8_t green = ((Pixels.Green(Color1) * (TotalSteps - Index)) + (Pixels.Green(Color2) * Index)) / TotalSteps;
+        uint8_t blue = ((Pixels.Blue(Color1) * (TotalSteps - Index)) + (Pixels.Blue(Color2) * Index)) / TotalSteps;
+        
+        Pixels.ColorSet(Pixels.Color(red, green, blue));
     }
 };
